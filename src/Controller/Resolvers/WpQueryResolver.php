a248fccad29956159e1885634b0e6da90fd13ff7
Clément Décou <clement.decou@ornikar.com>
2023-11-02 23:42:17 +0100

    fix: wp query resolver

--- src/Controller/Resolvers/WpQueryResolver.php
+++ src/Controller/Resolvers/WpQueryResolver.php
@@ -3,12 +3,14 @@
 namespace Simply\Mvc\Controller\Resolvers;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-final class WpQueryResolver implements ValueResolverInterface {
+final class WpQueryResolver implements ArgumentValueResolverInterface {
     public function supports(Request $request, ArgumentMetadata $argumentMetadata): bool {
-        return $argumentMetadata->getName() === 'WP_Query';
+        return $argumentMetadata->getName() === 'WP_Query'
+            || $argumentMetadata->getType() === \WP_Query::class
+            || $argumentMetadata->getName() === 'wp_query';
     }
 
     public function resolve(Request $request, ArgumentMetadata $argumentMetadata): \Generator {
